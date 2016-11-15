356dd5191e3b0aa9b2b48c89f09f57f51458a4b6
David Germain <davidispizza@gmail.com>
2016-11-15 17:53:04 -0500

    Customize/remove generic

--- navigation.js
+++ navigation.js
@@ -9,7 +9,6 @@ document.getElementById("navigation").innerHTML ='\
 			<span class="icon-bar"></span>\
 			<span class="icon-bar"></span>\
 		</button>\
-		<a class="navbar-brand" href="#">Start Bootstrap</a>\
 	</div>\
 	<!-- Collect the nav links, forms, and other content for toggling -->\
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
